49eb50f98b0d07a90d046c1ca7de83657dbfdcb1
Furkan Cengiz <76488131+furkancengiz@users.noreply.github.com>
2021-06-17 17:31:48 +0300

    Update ShortUrl.java

--- src/main/java/edu/kpi/developmentmethods/dto/ShortUrl.java
+++ src/main/java/edu/kpi/developmentmethods/dto/ShortUrl.java
@@ -4,23 +4,6 @@ package edu.kpi.developmentmethods.dto;
 // В даном примере мы используем экспериментальную в Java15 возможность — record classes,
 // чтобы сократить количество написаного кода
 public record Example (String title, int rating) {}
-public class ShortUrl {
 
-    private String shortUrl;
-
-    public ShortUrl() {
-    }
-
-    public ShortUrl(String shortUrl) {
-        this.shortUrl = shortUrl;
-    }
-
-    public String getShortUrl() {
-        return shortUrl;
-    }
-
-    public void setShortUrl(String shortUrl) {
-        this.shortUrl = shortUrl;
-    }
 
 }
